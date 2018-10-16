d980e040a130436a41d8260d066170a6062a0aa3
b-45 <bernard11ade@gmail.com>
2018-10-15 20:14:44 -0400

    DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 
 // Connect to MongoDb Atlas cluster
-mongoose
-  .connect( 
-    process.env.MONGO_URI,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+mongoose.connect( process.env.MONGO_URI,
+    { 
+      useNewUrlParser: true,
+    }
+  ).then(() => console.log("MongoDB connected"))
+   .catch(err => console.error(err));
+   mongoose.set('useCreateIndex', true)
 
 // Initialize Apollo/GraphQL Server
 const server = new ApolloServer({
