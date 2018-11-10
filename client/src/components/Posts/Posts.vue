22ab25958f015c01caf396c43e382eac2b52be73
b-45 <bernard11ade@gmail.com>
2018-11-10 13:55:31 -0500

    modified posts UI

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -2,25 +2,25 @@
   <ul class="list-reset" v-if="infiniteScrollPosts">
     <li class="flex justify-between bg-white rounded-sm select-none cursor-pointer shadow border-2 border-transparent hover:border-grey-light focus:outline-none focus:border-grey-light xs:pr-4 mb-4" v-for="post in infiniteScrollPosts.posts" :key="post._id">
       <!-- image -->
-      <div class="w-1/6 xs:hidden md:block lg:block">
-        <img class="block h-auto w-full rounded-l-sm" :src="post.thumbnail" alt="">
+      <div class="w-1/4 xs:hidden md:block lg:block">
+        <img class="block h-full w-full rounded-l-sm" :src="post.thumbnail" alt="">
       </div>
       <!-- title -->
-      <div class="flex flex-col justify-between w-4/6 pl-2">
-        <div>
-          <span class="font-medium text-grey-darker">{{post.title}}</span>
+      <div class="flex flex-col flex-1 justify-between w-1/2 ml-2">
+        <div class="mt-2">
+          <span class="font-medium text-grey-darker trunc">{{post.title}}</span>
         </div>
-        <div class="flex flex-wrap mb-2 text-xs ">
-          <span class="text-grey-dark mr-4">{{post.createdBy.username}}</span>
-          <span class="mr-4">·</span>
+        <div class="flex flex-wrap mb-2 text-xs">
+          <span class="text-grey-dark mr-2">{{post.createdBy.username}}</span>
+          <span class="mr-2">·</span>
           <span class="mr-1 text-green-dark">{{post.likeRatio}}</span>
-          <span class="text-grey-dark mr-4">like-ratio</span>
-          <span class="mr-4">·</span>
+          <span class="text-grey-dark mr-2">like-ratio</span>
+          <span class="mr-2">·</span>
           <span class="text-grey-dark">{{formatCreatedDate(post.createdDate)}}</span>
         </div>
       </div>
 
-      <div class="flex flex-shrink items-center p-2 w-1/6 opacity-50 xs:hidden md:flex  lg:flex">
+      <div class="flex justify-end items-center w-1/6 opacity-50">
         <span class="text-4xl mr-1 text-grey-darkest font-thin">{{post.duration}}</span>
         <span class="text-sm text-grey-dark">mins</span>
       </div>
@@ -83,5 +83,7 @@ export default {
   }
 };
 </script>
+
+
 
  
